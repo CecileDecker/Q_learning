4534ba61a1eb11850e30931ca4bfe75ed1cb0650
Cécile D <62743249+CecileDecker@users.noreply.github.com>
2023-09-03 12:36:49 +0200

    New advancement
    Still need to be cleaned

--- robust_q_learning_v3.py
+++ robust_q_learning_v3.py
@@ -1,4 +1,3 @@
-
 ############################################################################################
 
 import numpy as np
@@ -6,9 +5,9 @@ from tqdm import tqdm
 from scipy.optimize import minimize
 import copy
 
-# Classical Q learning #
+# Robust Q learning (in finite spaces) #
 
-def q_learning(X, A, r, P_0, alpha, x_0, eps_greedy = 0.05, Nr_iter = 1000, gamma_t_tilde = lambda t: 1/(t+1), Q_0 = None):
+def robust_q_learning_v2(X, A, r, P_0, p_0, alpha, x_0, k_0, eps_greedy = 0.05, Nr_iter = 1000, gamma_t_tilde = lambda t: 1/(t+1), Q_0 = None):
     """
     Parameters
     ----------
@@ -18,8 +17,10 @@ def q_learning(X, A, r, P_0, alpha, x_0, eps_greedy = 0.05, Nr_iter = 1000, gamm
         A list or numpy array containing all actions
     r : function
         Reward function r(x,a,y) depending on state-action-state.
-    P_0 : function
-        function P_0(x,a) that creates a new random variabe in dependence of state and action
+    P_0 : numpy.ndarray
+        P_0(x,a) list or numpy array of functions that creates a new random variabe in dependence of state and action
+    p_0 : numpy.ndarray
+        p_0(x,a,y) list of the probability distributions
     alpha : float
         Discounting rate.
     x_0 : numpy.ndarray
@@ -48,6 +49,7 @@ def q_learning(X, A, r, P_0, alpha, x_0, eps_greedy = 0.05, Nr_iter = 1000, gamm
     
     # Initialize the Visits matrix
     Visits = np.zeros([len(X), len(A)])
+    #Indic  = np.zeros([len(X), len(A)])
 
     # Catch A and X as lists type
     if np.ndim(A) > 1:
@@ -71,30 +73,54 @@ def q_learning(X, A, r, P_0, alpha, x_0, eps_greedy = 0.05, Nr_iter = 1000, gamm
 
     # Define the a_t function
     def a_t(y):
-        # eps_bound = 1-(t/Nr_iter)*(eps_greedy)
         eps_bound = eps_greedy
         unif      = np.random.uniform(0)
         return (unif > eps_bound) * A[np.argmax(Q[x_index(y), :])] + (unif <= eps_bound) * rng.choice(A)
     
-    # Define the gamma_t function
-    def gamma_t(j, v):
-        return gamma_t_tilde(v) * j 
+    # Define the P_ function
+    def P_(x, a, k):
+        unif      = np.random.uniform(0)
+        if (unif > eps_greedy):
+            return P_0[k](x, a)
+        else:
+            return rng.choice(X)
+
+    # Define the selection of the k-transition kernel
+    def k_t(x, a):
+        K_ = []
+        for k in range(len(P_0)):
+            K_ += [np.sum([(f(x, a, y)) * p_0[k](x, a, y) for y in X])]
+        k = np.argmin(np.array(K_))
+        return(k)
     
     # Set initial value
-    Y_0 = x_0
+    k_0 = k_0
+    Y_0 = [x_0 for P in P_0] # The markov decision processes
     # Iterations of Q and Visits and States
     for t in tqdm(range(Nr_iter)):
 
-        
-        x , a        = Y_0, a_t(Y_0)
+        A_0 = []
+        for y_0 in Y_0:
+            A_0 += [a_t(y_0)]
+
+        x, a         = Y_0[k_0], A_0[k_0]
+
+        Y_1 = []
+        for k in range(len(Y_0)):
+            Y_1 += [P_(Y_0[k], A_0[k], k)] # Update the Markov decision processes
+
+        k_1          = k_t(x, a)
+        y_1          = Y_1[k_1] # Get the next state
         Q_old        = copy.deepcopy(Q)
-        j_xa         = 1                     # Because in the non-robust case, (x, a) is literally chosen to be equal to (Y_t, a_t(Y_))
         x_ind, a_ind = x_index(x), a_index(a)
-        Y_1          = P_0(x, a) # Get the next state, with a bit of randomness
 
         # Do the update of Q
-        Q[x_ind, a_ind] = Q_old[x_ind, a_ind] + gamma_t(j_xa, Visits[x_ind, a_ind]) * (f(x, a, Y_1) - Q_old[x_ind, a_ind])
-        Visits[x_ind, a_ind] += 1            # Update the visits matrix
-        Y_0 = Y_1                            # Update the state
+        Q[x_ind, a_ind] = Q_old[x_ind, a_ind] +  gamma_t_tilde(Visits[x_ind, a_ind]) * (f(x, a, y_1) - Q_old[x_ind, a_ind])
+        Visits[x_ind, a_ind] += 1 # Update the visits matrix
+        
+        # Update the index of the markov decision process we consider next step
+        k_0 = k_1
+        # Update the Markov decision processes
+        Y_0 = [y_1 for P in P_0]
     
-    return Q, Visits
+    return Q, Visits 
